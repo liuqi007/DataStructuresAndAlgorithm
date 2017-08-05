cde3e511a2749f87839bfa2ca7d48c2435a85286
liuqi_it <liuqi_it@163.com>
2017-08-05 20:54:52 +0800

    first date ...............

---
+++ datastructures/src/com/fs/datastructures/SingleLinkedList.java
@@ -0,0 +1,10 @@
+package com.fs.datastructures;
+
+/**
+ * Created by Administrator on 2017/8/5 0005.
+ */
+public class SingleLinkedList {
+    public static void main(String args[]){
+        System.out.println("11");;
+    }
+}
